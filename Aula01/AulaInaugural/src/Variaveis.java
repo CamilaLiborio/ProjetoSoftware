d9f90de250f8d1a330fae2ff113edc43bce284cb
Camila Libório Santos <camilaliborio33@gmail.com>
2026-02-10 10:15:25 -0300

    .

---
+++ Aula01/AulaInaugural/src/Variaveis.java
@@ -0,0 +1,15 @@
+public class Variaveis {
+    public static void main(String[] args) {
+
+        int idade = 35;
+
+        double altura = 1.00;
+        double peso = 85.90;
+
+        char genero = 'F'; //Cracteres - ''
+        String nome = "Fulano"; // String - ""
+
+        System.out.println("Nome: " + nome + " Idade:" +idade +);
+
+    }
+}
