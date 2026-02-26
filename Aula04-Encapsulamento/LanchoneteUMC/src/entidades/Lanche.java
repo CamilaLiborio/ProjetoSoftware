89005797def1aba40fb8a9b713a814ed75083f19
CamilaLiborio <camilaliborio33@gmail.com>
2026-02-26 10:59:38 -0300

    .

---
+++ Aula04-Encapsulamento/LanchoneteUMC/src/entidades/Lanche.java
@@ -0,0 +1,66 @@
+package entidades;
+
+public class Lanche {
+    private String nome;
+    private String descricao;
+    private double valor;
+    private int estoque;
+    private boolean disponivel;
+
+    //Construtor
+    public Lanche(String nome, String descricao, double valor, int estoque) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.estoque = estoque;
+        this.disponivel = estoque > 0;
+
+    }
+
+    //Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    //Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+
+
+    //toString = usado para mostrar os atributos da classe
+    @Override
+    public String toString() {
+        return "Lanche{" +
+                "Nome='" + nome +
+                "\n Descricao: '" + descricao +
+                "\n Valor: R$" + valor +
+                "\n Estoque: " + estoque +
+                "\n Disponivel: " + disponivel ;
+    }
+}
