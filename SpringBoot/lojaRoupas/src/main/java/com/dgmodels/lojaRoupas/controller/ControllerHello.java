3ab53513e5b0e7a9d6d544d9d0bcb1c645c93b97
CamilaLiborio <camilaliborio33@gmail.com>
2026-04-30 10:50:58 -0300

    .

---
+++ SpringBoot/lojaRoupas/src/main/java/com/dgmodels/lojaRoupas/controller/ControllerHello.java
@@ -0,0 +1,16 @@
+package com.dgmodels.lojaRoupas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class ControllerHello {
+
+    @GetMapping
+    public String sayHello(){
+        return "Hello World";
+    }
+
+}
