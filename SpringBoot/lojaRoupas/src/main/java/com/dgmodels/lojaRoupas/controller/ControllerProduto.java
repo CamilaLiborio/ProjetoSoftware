196d3ecdc2ac647daf33bbee478d54e49becf2b1
CamilaLiborio <camilaliborio33@gmail.com>
2026-05-07 10:37:49 -0300

    .

--- SpringBoot/lojaRoupas/src/main/java/com/dgmodels/lojaRoupas/controller/ControllerProduto.java
+++ SpringBoot/lojaRoupas/src/main/java/com/dgmodels/lojaRoupas/controller/ControllerProduto.java
@@ -2,19 +2,29 @@ package com.dgmodels.lojaRoupas.controller;
 
 
 import com.dgmodels.lojaRoupas.model.DadosCadastroRoupa;
+import com.dgmodels.lojaRoupas.model.Roupa;
+import com.dgmodels.lojaRoupas.repository.RoupaRepository;
 import jdk.jfr.Registered;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping ("/roupa")
 public class ControllerProduto {
 
+    @Autowired
+    private RoupaRepository roupaRepository; // esse roupaRepository possui todos os métodos: Save, Find (buscar e recuperar dados do BD), Delete.
+
     @PostMapping
     public void cadastrarProduto(@RequestBody DadosCadastroRoupa dados ){
         System.out.println(dados);
+            roupaRepository.save(new Roupa(dados));
+    }
 
+    @GetMapping
+    public List<Roupa> listarProdutos(){
+        return roupaRepository.findAll();
     }
 }
---
+++ SpringBoot/lojaRoupas/src/main/java/com/dgmodels/lojaRoupas/repository/RoupaRepository.java
@@ -0,0 +1,10 @@
+package com.dgmodels.lojaRoupas.repository;
+
+
+import com.dgmodels.lojaRoupas.model.Roupa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoupaRepository extends JpaRepository<Roupa, Long> {
+
+
+}
